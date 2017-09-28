a5ca67175707a51dc7d48274c2c9950b964a335b
Laura Pierson Wadden <laura@stembolt.com>
2017-09-28 12:52:36 +0200

    Get around both sets of prompts for rake db:seed task
    The solidus_auth_devise gem has a few prompts that must be answered.

--- setup-database-and-run-solidus.sh
+++ setup-database-and-run-solidus.sh
@@ -2,6 +2,7 @@
 
 bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rake db:seed
+echo "no" | AUTO_ACCEPT=true bundle exec rake db:seed
+bundle exec rake spree_sample:load
 
 bundle exec rails server
