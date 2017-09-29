49dcc772bf33e0d574e2e660703cbfd69367ca79
Laura Pierson Wadden <laura@stembolt.com>
2017-09-29 09:54:05 +0200

    Precompile assets and serve static files
    Note: setting for serving static files is different in
    rails 4 and rails 5.

--- setup-database-and-run-solidus.sh
+++ setup-database-and-run-solidus.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+export RAILS_ENV=production
+export RAILS_SERVE_STATIC_FILES=true
+
 bundle exec rake db:create
 bundle exec rake db:migrate
 echo "no" | AUTO_ACCEPT=true bundle exec rake db:seed
+bundle exec rake assets:precompile
 
 bundle exec rails server
