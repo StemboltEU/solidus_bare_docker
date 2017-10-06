8c3bbf33ee0c5747c9145dc531c60553fed6468f
Martin Meyerhoff <martin@stembolt.com>
2017-10-06 16:13:34 +0200

    Only run rake once
    Rake loads the entire application. If we specify the tasks in one go,
    we save two Rails bootups on container startup.

--- bin/setup-database-and-run-solidus.sh
+++ bin/setup-database-and-run-solidus.sh
@@ -3,11 +3,6 @@
 export RAILS_ENV=production
 export RAILS_SERVE_STATIC_FILES=true
 
-bundle exec rake db:create
-bundle exec rake db:migrate
-
-# TODO: we should only call 'rake db:seed' one time. the seeds.rb file does
-# it's own check if it's already been seeded, but check should really be here.
-AUTO_ACCEPT=true bundle exec rake db:seed
+AUTO_ACCEPT=true bundle exec rake db:create db:migrate db:seed
 
 bundle exec rails server
